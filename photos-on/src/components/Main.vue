7428515ca335eaa72a28f8a206420b2696d1ba53
Eliel Maia <elieljuniorm@gmail.com>
2021-12-02 00:09:09 -0300

    testando resposta

--- photos-on/src/components/Main.vue
+++ photos-on/src/components/Main.vue
@@ -16,7 +16,7 @@ import { createClient } from 'pexels';
 
 const client = createClient('563492ad6f91700001000001e6d8a5315f0240f3902236ca5b3fb1b5');
 const query = 'Nature';
-client.photos.search({ query }).then(photos => console.log(photos));
+client.photos.search({ query }).then(photos => {console.log(photos)});
 
 export default {
 
@@ -24,7 +24,10 @@ export default {
       
     return{
       tituloPag: 'Photos On',
-      photos:[]
+      photos:[{
+        src:''
+      }
+      ]
     }
   },
 }
