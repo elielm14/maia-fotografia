8c10ea11f267c0a59554bed4f8489d9890b2d1f3
Eliel Maia <elieljuniorm@gmail.com>
2021-12-08 19:16:31 -0300

    incluindo formulario

--- photos-on/src/components/Formulario.vue
+++ photos-on/src/components/Formulario.vue
@@ -2,8 +2,11 @@
 
     <div class="from">
 
-        <label for="">Nome e Sobrenome</label>
-        <input type="text" name="nomeSobrenome" id="nomeSobrenome">
+        <label for="Nome e Sobrenome">Nome e Sobrenome</label>
+        <input type="text" name="nomeSobrenome" id="nomeSobrenome" placeholder="Nome e Sobrenome">
+
+        <label for="E-mail">E-mail</label>
+        <input type="email" name="email" id="email" placeholder="Digite seu E-mail">
 
     </div>
 
--- photos-on/src/views/Contact.vue
+++ photos-on/src/views/Contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="contact">
     <Header/>
-
+    <Formulario/>
     <Footer/>
   </div>
 </template>
@@ -9,12 +9,14 @@
 <script>
 import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
+import Formulario from '../components/Formulario.vue'
 
 export default {
   name: 'Contact',
   components: {
     Header,
-    Footer
+    Footer,
+    Formulario
   }
 }
 </script>
