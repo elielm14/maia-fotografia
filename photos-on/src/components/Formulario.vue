20179346772ee4a21989afdc59abe228cc31fee9
Eliel Maia <elieljuniorm@gmail.com>
2021-12-08 21:14:47 -0300

    adicionando input

--- photos-on/src/components/Formulario.vue
+++ photos-on/src/components/Formulario.vue
@@ -2,11 +2,16 @@
 
     <div class="from">
 
-        <label for="Nome e Sobrenome">Nome e Sobrenome</label>
-        <input type="text" name="nomeSobrenome" id="nomeSobrenome" placeholder="Nome e Sobrenome">
-
-        <label for="E-mail">E-mail</label>
-        <input type="email" name="email" id="email" placeholder="Digite seu E-mail">
+        <form action="">
+            <label for="Nome e Sobrenome">Nome e Sobrenome</label>
+            <input type="text" name="nomeSobrenome" id="nomeSobrenome" placeholder="Nome e Sobrenome">
+            <label for="E-mail">E-mail</label>
+            <input type="email" name="email" id="email" placeholder="Digite seu E-mail">
+            <label for="telefone">Telefone</label>
+            <input type="tel" name="telefone" id="telefone">
+            <label for="mensagem">Mensagem</label>
+            <textarea name="mensagem" id="mensagem" cols="70" rows="10" class="input-padrao" required></textarea>
+        </form>
 
     </div>
 
