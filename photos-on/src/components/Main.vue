937273e68b2502c6c5d6e0c76aee4c8cd6815504
Eliel Maia <elieljuniorm@gmail.com>
2021-12-10 23:45:35 -0300

    atulizando favicon e criando botao

--- photos-on/src/components/Main.vue
+++ photos-on/src/components/Main.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="main">
 
+    <button v-on:click="proximaPagina">Proxima página</button>
+    <button v-on:click="paginaAnterior">Página Anterior</button>
+
     <ul>
 
       <li class="lista-fotos-item" v-for="imagem in imagens" :key="imagem.id">
 
       <!--   <img :src="imagem.src.landscape" :alt="imagem.titulo"> -->
 
-        <progressive-img :src="imagem.src.landscape" :placeholder="imagem.src.small" :blur="30" />
+        <progressive-img :src="imagem.src.landscape" :placeholder="imagem.src.small" :blur="30"/>
 
       </li>
 
@@ -27,30 +30,42 @@ export default {
     return{
 
       imagens:[],
-      pagina: [1]
+      pagina: 80
     }
   },
 
   async created(){
+    await this.carregaPagina()
+  },
 
+  methods:{
+        proximaPagina: function(){
+        this.pagina = this.pagina + 1
+        console.log(this.pagina);
+        this.carregaPagina()
+    },
+    paginaAnterior: function(){
+        this.pagina = this.pagina - 1
+        console.log(this.pagina);
+        this.carregaPagina()
+    },
+
+    async carregaPagina(){
     try {
       const client = createClient('563492ad6f91700001000001e6d8a5315f0240f3902236ca5b3fb1b5');
       const query = 'casamento';
-      client.photos.search({ query, locale: 'pt-BR', per_page: 8, page: this.pagina }).then(response => {
+      client.photos.search({ query, locale: 'pt-BR', per_page: 8, page: [this.pagina] }).then(response => {
         this.imagens = response.photos
       });
 
     } catch (error) {
       console.log(error)
     }
-  },
-
-  methods:{
+    }
   }
 }
 </script>
 
-<!-- scoped para uso de estilo exclusivo -->
 <style scoped>
 .main{
 text-align: center;
